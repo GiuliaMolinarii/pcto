93eb03f2bf6915d9ae1bf5ba037dcc7dfb8ef51b
GiuliaMolinarii <124669203+GiuliaMolinarii@users.noreply.github.com>
2023-02-07 11:05:56 +0100

    Add files via upload

---
+++ giulipcto.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 21;
+    int b = 2;
+    int somma = a + b;
+int divisione = a / b ;
+int percentuale = a % b;
+
+
+    printf("la divisione di 21 e 2 è : %d\n " , divisione );
+    printf (" la percentuale di 21 e 2 è : %d\n", percentuale);
+}
