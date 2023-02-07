8d22a193866bcd3f84958c3c3a3a5641cfbbb034
GiuliaMolinarii <124669203+GiuliaMolinarii@users.noreply.github.com>
2023-02-07 10:28:52 +0100

    Add files via upload

---
+++ fileGiulia.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int numero = 20;
+double banana = -5614584; //%f
+float aereoplano = 5.6; //%f
+char neymarjr = 'c'; //%c
+
+printf("numero: %d\n",numero);
+printf ("banana: %f\n",banana);
+printf("aereoplano: %f\n",aereoplano);
+printf("neymarjr: %c\n",neymarjr);
+
+}
---
+++ gm.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+    char x = 'a';
+    char y = 'b';
+    int num = 56
+
+    printf("x era: %c\n" ,x);
+    printf("y era: %c\n",y);
+    printf("num era : %d\n",num);
+    printf ("inserisci due caratteri e un intero\n"):
+    scanf ("%c %c",&x, &y);
+    printf("x è diventato: %d\n", x);
+    printf( "y è diventato: %d\n", y);
+   
+} 
