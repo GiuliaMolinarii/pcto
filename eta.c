83ddc795ea1b46ab648824304ae60495769c9b40
GiuliaMolinarii <124669203+GiuliaMolinarii@users.noreply.github.com>
2023-02-07 14:56:34 +0100

    Add files via upload

---
+++ eta.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+ int a;
+ 
+ 
+  printf ("inserisci eta\n" );
+    scanf ("%d" , &a); 
+    if (a > 18)
+    { 
+        printf ( "a è maggiore di 18\n");
+    
+    }
+else if ( a < 18 )
+{
+    printf ("a è minore di 18 \n");
+}
+}
